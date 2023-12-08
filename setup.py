24e3ae0a6e17caebe34a659754f9546cb7d0998e
Arifian6 <dwifaputra16@gmail.com>
2023-12-08 17:33:27 +0700

    commit pertama

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Dwifa_Putra',
+    version='0.1.0',
+    author='Arifian6',
+    author_email='dwifaputra16@gmail.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
